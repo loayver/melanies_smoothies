2bb5e684a96656d483173406e1a851149f465fe4
Loayver <59124853+loayver@users.noreply.github.com>
2026-09-16 22:59:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,12 +21,12 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 # convert to pandas
-pd_df = my_dataframe.to_pandas()
-st.dataframe=(pd_df)
+#pd_df = my_dataframe.to_pandas()
+#st.dataframe=(pd_df)
 #st.stop()
 
 ingredients_list = None
@@ -48,7 +48,7 @@ if ingredients_list:
       
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
-        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # st.write(my_insert_stmt)
 time_to_insert = st.button('Submit Order')
