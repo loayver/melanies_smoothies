15da1a64a058b237f8ef2e12f9a8a32acf81fe8a
Loayver <59124853+loayver@users.noreply.github.com>
2026-09-16 23:22:11 -0400

    Collect dataframe from Snowflake session
    Collect data from the Snowflake session before displaying it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.write("The name on your Smoothie: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')).collect()
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
