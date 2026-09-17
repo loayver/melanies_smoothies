b148e6348368bbcf7994feffae9f09f33531277d
Loayver <59124853+loayver@users.noreply.github.com>
2026-09-16 22:05:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
 # st.write(my_insert_stmt)
 time_to_insert = st.button('Submit Order')
